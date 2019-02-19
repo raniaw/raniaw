f2605dadbddf0070da4a2ea30433dbcff4ff4fe9
raniaw <raniaw@web.de>
2019-02-19 09:43:50 +0100

    update

--- lesson17/js/script.js
+++ lesson17/js/script.js
@@ -1,4 +1,28 @@
-// alert("Welcome to JS ");
+
+
+function changestyle() {
+    document.getElementById('myDiv').style.transition='all 3s';
+    // document.getElementById('first').style.border = '3px dotted green';
+    // document.getElementById('first').style.borderRadius = '15px';
+    // document.getElementById('first').style.color = 'red';
+    // document.getElementById('first').style.fontFamily = 'Century Gothic, Verdana, Geneva, Tahoma, Sans-Serif';
+
+    // for (var i = 0; i <= 6; i++) {
+
+    //     document.getElementsByTagName('button')[i].style.backgroundColor = "yellow";
+    // }
+    // // console.log(getElementsByTagName('button').style.background);
+    // var newwidth = "200px;"
+    // var hello = "Hello";
+    // alert(hello + " " + newwidth);
+}
+
+function welcome(fname,lname){
+    var welcome="Welcome ";
+    document.getElementById('first').innerHTML=welcome+ " "+ fname+" "+ lname;
+    
+}
+
 
 function showhide() {
     if (document.getElementById('img').style.display == 'block') {
@@ -8,27 +32,4 @@ function showhide() {
         document.getElementById('img').style.display = 'block';
         document.getElementById('showhide').innerHTML = 'Hide';
     }
-}
-
-function changestyle() {
-    document.getElementById('first').style.background = 'white';
-    document.getElementById('first').style.border = '3px dotted green';
-    document.getElementById('first').style.borderRadius = '15px';
-    document.getElementById('first').style.color = 'red';
-    document.getElementById('first').style.fontFamily = 'Century Gothic, Verdana, Geneva, Tahoma, Sans-Serif';
-
-    for (var i = 0; i <= 6; i++) {
-
-        document.getElementsByTagName('button')[i].style.backgroundColor = "yellow";
-    }
-    // console.log(getElementsByTagName('button').style.background);
-    var newwidth = "200px;"
-    var hello = "Hello";
-    alert(hello + " " + newwidth);
-}
-
-function welcome(fname,lname){
-    var welcome="Welcome ";
-    document.getElementById('first').innerHTML=welcome+ " "+ fname+" "+ lname;
-    
 }
