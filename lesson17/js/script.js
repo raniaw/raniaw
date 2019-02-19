e29144d7c27040e655cd55de63046d0666bd9c0b
raniaw <raniaw@web.de>
2019-02-19 11:23:27 +0100

    update

--- lesson17/js/script.js
+++ lesson17/js/script.js
@@ -1,7 +1,9 @@
 
 
-function changestyle() {
+function styletransition() {
     document.getElementById('myDiv').style.transition='all 3s';
+    // document.getElementById('myDiv').style.transform="rotatey(50deg)";//rotate y-achse 50grad
+    
     // document.getElementById('first').style.border = '3px dotted green';
     // document.getElementById('first').style.borderRadius = '15px';
     // document.getElementById('first').style.color = 'red';
@@ -15,6 +17,15 @@ function changestyle() {
     // var newwidth = "200px;"
     // var hello = "Hello";
     // alert(hello + " " + newwidth);
+}
+
+function changestyle2(){
+    document.getElementById('myDiv').style.transform="rotate(720deg)"; // rotate 720grad
+    // document.getElementById('myDiv').style.transform="rotatex(70deg)";//rotate x-achse 70grad
+// document.getElementById('myDiv').style.transform="rotatey(50deg)";//rotate y-achse 50grad
+}
+function changeparagraph(){
+    document.getElementById("first").style.backgroundColor="#e2cdcd";
 }
 
 function welcome(fname,lname){
