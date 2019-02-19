13b0e09f844ea98fa13a4763fb926e1d2f05e337
raniaw <raniaw@web.de>
2019-02-19 13:15:16 +0100

    update

--- lesson17/js/script.js
+++ lesson17/js/script.js
@@ -24,8 +24,26 @@ function changestyle2(){
     // document.getElementById('myDiv').style.transform="rotatex(70deg)";//rotate x-achse 70grad
 // document.getElementById('myDiv').style.transform="rotatey(50deg)";//rotate y-achse 50grad
 }
+
+/***
+** this function change color of backgound by paragraph 
+
+** id="first" -> lightpink("#e2cdcd")
+** id="first" -> will be change as div with class='but'
+** class="para" first element-> circle border with borderRadius
+** class="para" second element-> white
+** class="para" third element-> yellow
+
+***/
 function changeparagraph(){
     document.getElementById("first").style.backgroundColor="#e2cdcd";
+
+    document.getElementsByClassName('para')[1].style.backgroundColor="white";
+    document.getElementsByClassName('para')[2].style.backgroundColor="yellow";
+    document.getElementsByClassName('para')[0].style.borderRadius="200px";
+    document.getElementById('first').innerHTML="<div class='but'> Now insted p is div-selector </div>"
+
+    
 }
 
 function welcome(fname,lname){
