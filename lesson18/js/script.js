0b40e8fcee7d9588b7f8bdf931237380a9b420b1
raniaw <raniaw@web.de>
2019-02-20 13:22:12 +0100

    update

--- lesson18/js/script.js
+++ lesson18/js/script.js
@@ -17,50 +17,70 @@ function third() {
 
     a = 6; 
     b = 4; 
-    c=a**b; 
-    var obj={a:"a", "b":b, "c":c};
+    c = a ** b; 
+    var obj =  {a:"a", "b":b, "c":c}; 
 
-    add=x+y;// x+=y <=> x=x+y
-    sub=x-y;//x-=y <=> x=x-y
-    mult=x*y;//x*=y <=> x=x*y
-    div=x-y;//x-=y <=> x=x-y
-    inc=++x;
-    dec=--y;
-    console.log(div);
+    add = x + y; // x+=y <=> x=x+y
+sub = x - y; //x-=y <=> x=x-y
+mult = x * y; //x*=y <=> x=x*y
+div = x - y; //x-=y <=> x=x-y
+inc = +  + x; 
+    dec = --y; 
+    console.log(div); 
     
-    window.alert("Result "+ z); 
-    document.getElementById('s-el').innerHTML = "Result of 'x/y=z':"+z;
-    document.getElementById('s-el').style.fontSize='20px';
-    document.getElementById('t-el').innerHTML = "Result of obj 'a^b=c': "+ obj.c;
-    document.getElementById('t-el').style.fontSize='20px';
-    document.getElementById('fr-el').innerHTML =
-    "add: "+add+"</br>"+
-    "sub: "+sub+"</br>"+
-    "mult: "+mult+"</br>"+
-    "div: "+div+"</br>"+
-    "inc: "+inc+"</br>"+
-    "dec: "+dec+"</br>";
+    window.alert("Result " + z); 
+    document.getElementById('s-el').innerHTML = "Result of 'x/y=z':" + z; 
+    document.getElementById('s-el').style.fontSize = '20px'; 
+    document.getElementById('t-el').innerHTML = "Result of obj 'a^b=c': " + obj.c; 
+    document.getElementById('t-el').style.fontSize = '20px'; 
+    document.getElementById('fr-el').innerHTML = 
+    "add: " + add + "</br>" + 
+    "sub: " + sub + "</br>" + 
+    "mult: " + mult + "</br>" + 
+    "div: " + div + "</br>" + 
+    "inc: " + inc + "</br>" + 
+    "dec: " + dec + "</br>"; 
 }
 
-function showname(){
-    var myname="Rania";
+function showfirstname() {
+    var firstname = "Rania"; 
 
-    document.getElementById('fv-el').innerHTML=myname; 
-    document.getElementById('fv-el').style.fontFamily="Gothic Centure, Verdana";
-    document.getElementById('fv-el').style.fontSize='30px';
+    document.getElementById('fr-el').innerHTML = firstname; 
+    document.getElementById('fr-el').style.fontFamily = "Gothic Centure, Verdana"; 
+    document.getElementById('fr-el').style.fontSize = '30px'; 
+    return firstname; 
 }
 function showhide() {
-    var mysurname="Wittenberg";
+    var myname = showfirstname(); 
+    var mysurname = "Wittenberg"; 
 
-    document.getElementById('fr-el').innerHTML=mysurname; 
-    document.getElementById('fr-el').style.fontFamily="Gothic Centure, Verdana";
-    document.getElementById('fr-el').style.fontSize='30px';
+    document.getElementById('fr-el').innerHTML = myname; 
+    document.getElementById('fv-el').innerHTML = mysurname; 
+    // document.getElementById('fv-el').style.fontFamily="Gothic Centure, Verdana";
+    document.getElementById('fv-el').style
+    document.getElementById('fv-el').style.fontSize = '30px'; 
 
-    if (document.getElementById('fr-el').style.display == 'block') {
-        document.getElementById('fr-el').style.display = 'none';    
-        document.getElementById('fr-el').innerHTML = "";
-    } else {
-        document.getElementById('fr-el').style.display = 'block';
-        document.getElementById('fr-el').innerHTML = mysurname;
+    if (document.getElementById('fv-el').style.display == 'block' && 
+    document.getElementById('fr-el').style.display == 'block') {
+        document.getElementById('fv-el').style.display = 'none'; 
+        document.getElementById('fr-el').style.display = 'none'; 
+        document.getElementById('fr-el').innerHTML = ""; 
+        document.getElementById('fv-el').innerHTML = ""; 
+    }else {
+        myname = showfirstname(); 
+        document.getElementById('fv-el').style.display = 'block'; 
+        document.getElementById('fr-el').style.display = 'block'; 
+        document.getElementById('fv-el').innerHTML =  mysurname; 
+        document.getElementById('fr-el').innerHTML = myname ; 
+        
+        // document.getElementById('fv-el').innerHTML = myname + " " + mysurname; 
     }
+}
+function showname() {
+    var firstname = "Rania"; 
+
+    document.getElementById('fr-el').innerHTML = firstname; 
+    document.getElementById('fr-el').style.fontFamily = "Gothic Centure, Verdana"; 
+    document.getElementById('fr-el').style.fontSize = '30px'; 
+
 }
