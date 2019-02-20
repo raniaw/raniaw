376324eb61a2d4c8a56147a6f0b8489e33ee375f
raniaw <raniaw@web.de>
2019-02-20 11:44:19 +0100

    update

--- lesson18/js/script.js
+++ lesson18/js/script.js
@@ -4,6 +4,8 @@ function first() {
 
 function second() {
     document.getElementById('f-el').innerHTML = ''; 
+    document.getElementById('fv-el').innerHTML = ''; 
+    document.getElementById('fr-el').innerHTML = ''; 
 
 }
 
@@ -18,9 +20,13 @@ function third() {
     c=a**b; 
     var obj={a:"a", "b":b, "c":c};
 
+    add=x+y;// x+=y <=> x=x+y
+    sub=x-y;//x-=y <=> x=x-y
+    mult=x*y;//x*=y <=> x=x*y
+    div=x-y;//x-=y <=> x=x-y
     inc=++x;
     dec=--y;
-  
+    console.log(div);
     
     window.alert("Result "+ z); 
     document.getElementById('s-el').innerHTML = "Result of 'x/y=z':"+z;
@@ -28,6 +34,33 @@ function third() {
     document.getElementById('t-el').innerHTML = "Result of obj 'a^b=c': "+ obj.c;
     document.getElementById('t-el').style.fontSize='20px';
     document.getElementById('fr-el').innerHTML =
+    "add: "+add+"</br>"+
+    "sub: "+sub+"</br>"+
+    "mult: "+mult+"</br>"+
+    "div: "+div+"</br>"+
     "inc: "+inc+"</br>"+
     "dec: "+dec+"</br>";
+}
+
+function showname(){
+    var myname="Rania";
+
+    document.getElementById('fv-el').innerHTML=myname; 
+    document.getElementById('fv-el').style.fontFamily="Gothic Centure, Verdana";
+    document.getElementById('fv-el').style.fontSize='30px';
+}
+function showhide() {
+    var mysurname="Wittenberg";
+
+    document.getElementById('fr-el').innerHTML=mysurname; 
+    document.getElementById('fr-el').style.fontFamily="Gothic Centure, Verdana";
+    document.getElementById('fr-el').style.fontSize='30px';
+
+    if (document.getElementById('fr-el').style.display == 'block') {
+        document.getElementById('fr-el').style.display = 'none';    
+        document.getElementById('fr-el').innerHTML = "";
+    } else {
+        document.getElementById('fr-el').style.display = 'block';
+        document.getElementById('fr-el').innerHTML = mysurname;
+    }
 }
